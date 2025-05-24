04a61477f141196b41b6a847fae06fc41a2918a8
sandesh <sandeshbhatta218@gmail.com>
2025-05-24 09:21:42 +0545

    Basic principles.

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,21 @@
-import './style.css'
-import { createRoot } from 'react-dom/client'
+import "./style.css";
+import { createRoot } from "react-dom/client";
 
-const root = createRoot(document.querySelector('#root'))
+const root = createRoot(document.querySelector("#root"));
+
+const toto = "there";
 
 root.render(
-    <h1>Hello React</h1>
-)
+  <>
+    <h1 style={{ color: "coral", backgroundColor: "floralwhite" }}>
+      Hello {toto}
+    </h1>
+    <p className="paragraph">
+      Lorem <strong>ipsum</strong> Lorem ipsum dolor sit, amet consectetur
+      adipisicing elit. <br />
+      Facilis, et! Eligendi perspiciatis porro accusantium nisi et. Iure dolore,
+      labore officiis pariatur a et distinctio quisquam voluptates, cumque
+      accusantium, accusamus officia.
+    </p>
+  </>
+);
